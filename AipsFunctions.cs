702ef04d7e3e77b6c99f8f8839cc788642d8a9d0
Vladimir <56363775+jpegie@users.noreply.github.com>
2023-04-11 01:39:06 +0500

    Добавляю реализацию DFT, FFT

---
+++ AipsFunctions.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+
+namespace Fourier
+{
+    public static class FourierFuncs
+    {
+        public static Complex[] DFT(Complex[] signal)
+        {
+            var length = signal.Length;
+            var dft_res = new Complex[length];
+            for (int k = 0; k < length; k++)
+            {
+                Complex sum = 0;
+                for (int n = 0; n < length; n++)
+                {
+                    sum += signal[n] * Complex.Exp(-2 * Math.PI * Complex.ImaginaryOne * k * n / length);
+                }
+                dft_res[k] = sum;
+            }
+            return BinaryInversePermutate(dft_res);
+        }
+
+        public static Complex[]? FFT(Complex[] signal)
+        {
+            if (signal.Length < 2)
+            {
+                return null;
+            };
+
+            var length = signal.Length / 2;
+            var x_even = new Complex[length];
+            var x_odd = new Complex[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                var even = signal[i];
+                var odd = signal[length + i];
+
+                x_even[i] = even + odd;
+                x_odd[i] = (even - odd) * GetW(length, i);
+            }
+
+            FFT(x_even);
+            FFT(x_odd);
+
+            for (int i = 0, j = 0; i < length; i++)
+            {
+                signal[j++] = x_even[i];
+                signal[j++] = x_odd[i];
+            }
+            return signal;
+        }
+
+        public static Complex[] BinaryInversePermutate(Complex[] permutated)
+        {
+            double factor = permutated.Length / 2;
+            for (var i = 0; i < permutated.Length; i++)
+            {
+                permutated[i] /= factor;
+            }
+            return permutated;
+        }
+
+        static Complex GetW(int length, int i)
+        {
+            var abs = -Math.PI / length;
+            var wBase = new Complex(Math.Cos(abs), Math.Sin(abs));
+            return Complex.Pow(wBase, i);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using MathNet.Numerics;
+using System.Numerics;
+using Fourier;
+
+class Program
+{
+    static string GetWordFromResult(Complex[] result)
+    {
+        var modulos = result.Select(compl => Complex.Abs(compl)).Take(7);
+        var roundedModulos = modulos.Select(modulo => Math.Round(modulo));
+        var symbolsCodes = roundedModulos.Select(modulo => (char)modulo);
+        var str = string.Join("", symbolsCodes);
+        return str;
+    }
+    static Complex[] GetSignalFromFile()
+    {
+        var signal = new StreamReader(@"C:\Users\Vladimir\Desktop\10.txt")
+            .ReadToEnd()
+            .Trim()
+            .Split("\n")
+            .Select(sig => float.Parse(sig));
+        var signalComplex = signal.Select(sig => new Complex(sig, 0))
+                                  .ToArray();
+        return signalComplex;
+    } 
+    public static void Main()
+    {
+        var signalComplex = GetSignalFromFile();
+
+        var dftRes = FourierFuncs.DFT(signalComplex);
+        var fftRes = FourierFuncs.FFT(signalComplex);
+        var bipRes = FourierFuncs.BinaryInversePermutate(fftRes!);
+
+        var dft_word = GetWordFromResult(dftRes);
+        var fft_word = GetWordFromResult(bipRes);
+
+        Console.WriteLine("Слово, полученное DFT: " + dft_word );
+        Console.WriteLine("Слово, полученное FFT: " + fft_word);
+    }
+}
